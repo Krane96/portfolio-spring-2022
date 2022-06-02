090d4b64eb350d930978b55bc1e891653d91d414
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-06-03 01:47:58 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,67 @@
 import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_epm4gih",
+        "template_39iq28a",
+        form.current,
+        "esey9fTY1Md03PE-Q"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
-    <section id="contact"></section>
-  )
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className="contact_icon" />
+            <h4>Email</h4>
+            <h5>martinkrane@hotmail.com</h5>
+            <a href="mailto:martinkrane96@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            rows="7"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
