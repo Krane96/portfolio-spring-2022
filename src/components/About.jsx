d59f3add1ec61879cdbd8339e3fb4ea7e7429b8b
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-06-07 19:28:31 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,6 +26,17 @@ const About = () => {
             </article>
           </div> */}
           <p>
+            Hello, My name is Martin Lian Krane. I currently reside in Bergen,
+            Norway and have been studying Frontend development for 2 years. My
+            program ends in June 2022. <br />
+            <br /> Besides the love for technology, I enjoy doing physical
+            activities, being active and exercising. I enjoy having intellectual
+            conversations as well as spending my time gaming, enjoying tv shows
+            and movies.
+            <br />
+            <br /> I have learned a lot from my short time as a programmer and
+            am eager to learn more. Got a good eye for details and I am a fast
+            learner.
           </p>
           <a href="#contact" className="btn btn-primary">
             Contact Me
