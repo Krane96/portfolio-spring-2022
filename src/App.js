3a75d508f8087f571163cd7412893404d9200c5d
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-06-08 16:00:28 +0200

    animation test

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 const App = () => {
   return (
     <>
-      <Header />
+      <div data-aos="fade-down">
+        <Header />
+      </div>
       <Nav />
-      <Experience />
+      <div data-aos="fade-right">
+        <Experience />
+      </div>
+      <div data-aos="fade-left">
       <Projects />
+      </div>
+      <div data-aos="fade-right">
+
       <About />
+      </div>
+      <div data-aos="fade-left">
       <Contact />
+      </div>
       <Footer />
     </>
   );
